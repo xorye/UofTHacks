7c8874a4014f0d07a97c549feb1eedc67c08e925
David <20326645+xorye@users.noreply.github.com>
2018-01-20 09:42:10 -0500

    Update gitignore and change if condition in Open_Security.py

--- Open_Security.py
+++ Open_Security.py
@@ -5,17 +5,17 @@ import numpy as np
 capture = cv2.VideoCapture(0)
 
 while True:
-    feed_on, frame = cap.read()
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    feed_on, frame = capture.read()
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     ### Object detection below
 
 
     # Output the video feed
     cv2.imshow("Video Feed", frame)
-    
+
     # Define how to exit and halt the video feed
-    if cv2.waitKey(0):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
