a932504fa20cb89bb1512917a1df41809df00ba9
Kavan <kavan_lam@outlook.com>
2018-01-20 11:11:24 -0500

    a

--- Open_Security.py
+++ Open_Security.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import tweepy
 
 # the video capture object (responsible for getting raw video data)
 capture = cv2.VideoCapture(0)
@@ -19,3 +20,18 @@ while True:
         break
 
 cv2.destroyAllWindows()
+
+'''
+# Api keys required
+CONSUMER_KEY ="VvPlmI6w4sdg3AdQ055nW0Mq6"
+CONSUMER_SECRET = "vlGOr8mwZly5FWucbVvqDarbiVYMv8GIk2uEuHvYXaFuo8ZMyO"
+ACCESS_KEY = "954725736790810627-0LIzKoQB26Ap4jgt3tS8Fiao3yZAjAM"
+ACCESS_SECRET = "XPgIM9G0uNhGxzCMMoEm3qiplMOuwGt4f4iSobEIhsyCB"
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+# Send a tweet
+api.update_status("UofT Hack")
+'''
