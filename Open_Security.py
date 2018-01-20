db571ec753834ecaf1ef8cdae94afa4d9d40a1d4
Kavan <kavan_lam@outlook.com>
2018-01-20 09:27:42 -0500

    added main file

---
+++ Open_Security.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+# the video capture object (responsible for getting raw video data)
+capture = cv2.VideoCapture(0)
+
+while True:
+    feed_on, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ### Object detection below
+
+
+    # Output the video feed
+    cv2.imshow("Video Feed", frame)
+    
+    # Define how to exit and halt the video feed
+    if cv2.waitKey(0):
+        break
+
+cv2.destroyAllWindows()
