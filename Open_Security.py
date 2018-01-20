93c4510674f2991edd18d89cfdf818a4187bd7c2
Kavan <kavan_lam@outlook.com>
2018-01-20 16:11:19 -0500

    a

--- Open_Security.py
+++ Open_Security.py
@@ -1,37 +1,180 @@
 import cv2
-import numpy as np
 import tweepy
-
-# the video capture object (responsible for getting raw video data)
-capture = cv2.VideoCapture(0)
-
-while True:
-    feed_on, frame = capture.read()
-    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    ### Object detection below
+from tkinter import *
+import numpy as np
 
 
-    # Output the video feed
-    cv2.imshow("Video Feed", frame)
+class Output:
+    def __init__(self, security_level, object_detection):
+        self.security_level = security_level
+        self.object_detection = object_detection
 
-    # Define how to exit and halt the video feed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    """
+    Main
+    - start_video_processing takes care of video processing and object detection
+    """
+    def start_video_processing(self):
 
-cv2.destroyAllWindows()
+        # the video capture object (responsible for getting raw video data)
+        capture = cv2.VideoCapture(0)
 
-'''
-# Api keys required
-CONSUMER_KEY ="VvPlmI6w4sdg3AdQ055nW0Mq6"
-CONSUMER_SECRET = "vlGOr8mwZly5FWucbVvqDarbiVYMv8GIk2uEuHvYXaFuo8ZMyO"
-ACCESS_KEY = "954725736790810627-0LIzKoQB26Ap4jgt3tS8Fiao3yZAjAM"
-ACCESS_SECRET = "XPgIM9G0uNhGxzCMMoEm3qiplMOuwGt4f4iSobEIhsyCB"
+        # the objects we want to detect (cascades)
+        gun_cascade = cv2.CascadeClassifier("cascades/m9.xml")
+        knife_cascade = cv2.CascadeClassifier("cascades/knife.xml")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+        object_in_view_frames = 0  # this is keep track of how many frames a gun is in view for
+        object_in_view_already_detected = False
 
-# Send a tweet
-api.update_status("UofT Hack")
-'''
+        while True:
+            feed_on, frame = capture.read()
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Scan for guns and store the result if needed
+            if self.object_detection:
+                if self.security_level == 1:  # 1 == high security
+                    gun_in_view = gun_cascade.detectMultiScale(frame, 2, 2)
+                    knife_in_view = knife_cascade.detectMultiScale(frame, 2, 2)
+                else:
+                    gun_in_view = ()
+                    knife_in_view = knife_cascade.detectMultiScale(frame, 2, 2)
+            else:
+                gun_in_view = ()
+                knife_in_view = ()
+
+            # Outline any guns or knives in view and act accordingly
+            if gun_in_view != () or knife_in_view != ():  # if not gun is detected then gun_in_view should be set to an empty tuple
+                for (x, y, w, h) in gun_in_view:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                for (x, y, w, h) in knife_in_view:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                object_in_view_frames += 1
+                if object_in_view_frames > 7 and not object_in_view_already_detected:
+                    cv2.imwrite("positive_detections/alert.jpg", frame)
+                    #send_tweet() ########################################### remove after
+                    object_in_view_already_detected = True
+            else:
+                object_in_view_frames = 0
+                object_in_view_already_detected = False
+
+            # Output the video feed
+            cv2.imshow("Video Feed", frame)
+
+            # Define how to exit and halt the video feed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+        capture.release()
+
+    """
+    send_tweet
+    - Resonsible for sending a tweet when a gun us detected
+    """
+    def send_tweet(self):
+        # Api keys required
+        consumer_key = "VvPlmI6w4sdg3AdQ055nW0Mq6"
+        consumer_secret = "vlGOr8mwZly5FWucbVvqDarbiVYMv8GIk2uEuHvYXaFuo8ZMyO"
+        access_key = "954725736790810627-0LIzKoQB26Ap4jgt3tS8Fiao3yZAjAM"
+        access_secret = "XPgIM9G0uNhGxzCMMoEm3qiplMOuwGt4f4iSobEIhsyCB"
+
+        # api setup
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+        api = tweepy.API(auth)
+
+        # Send a tweet
+        api.update_with_media("positive_detections/alert.jpg", "Alert: dsbhvdsjbvsjdbvjsdsj "
+                                                               "(THIS IS JUST A TEST AND IS NOT REAL)")
+
+
+
+class OptionMenu:
+    def __init__(self):
+        # Creating output screens and basic setup
+        self.master_screen = Tk()
+        self.master_screen.minsize(width=600, height=400)
+        self.master_screen.maxsize(width=600, height=400)
+        self.master_screen.title("Open Security")
+        self.master_screen.configure(bg="Gray")
+        # variables to keep track of user preference
+        self.security_level_input = 1  # 1 = High Security(guns, knife)   2 = Low Security(just knife)
+        self.object_detection_input = True  # toggle the object detection feature
+        security_message = self.determine_option_message()
+        self.security_label = Label(self.master_screen, text=security_message, bg="Gray", fg="green", font=("Helvetica", 20))
+        self.security_label.place(x=110, y=270)
+
+    def start(self):
+        self.master_screen.destroy()
+
+    def exit_app(self):
+        self.master_screen.destroy()
+        quit()
+
+    def set_to_low_security(self):
+        self.security_level_input = 2
+        self.display_option_message()
+
+    def set_to_high_security(self):
+        self.security_level_input = 1
+        self.display_option_message()
+
+    def toggle_object_detection(self):
+        self.object_detection_input = not self.object_detection_input
+        self.display_option_message()
+
+    def determine_option_message(self):
+        if not self.object_detection_input:
+            security_message = "Selected Option = All object detection is off"
+        elif self.security_level_input == 1:
+            security_message = "Selected Option = High Security"
+        else:
+            security_message = "Selected Option = Low Security"
+        return security_message
+
+    def display_option_message(self):
+        security_message = self.determine_option_message()
+        self.security_label.destroy()
+        self.security_label = Label(self.master_screen, text=security_message, bg="Gray", fg="green",
+                                    font=("Helvetica", 20))
+        if security_message != "Selected Option = All object detection is off":
+            self.security_label.place(x=110, y=270)
+        else:
+            self.security_label.place(x=30, y=270)
+
+    def options_menu(self):
+        # Declaring all widgets
+        # Labels
+        app_title = Label(self.master_screen, text="Open Security", bg="Gray", fg="green", font=("Helvetica", 30))
+        # buttons
+        start_button = Button(self.master_screen, text="Start", command=self.start, font=("Helvetica", 15),
+                             height=1, width=10)
+        exit_button = Button(self.master_screen, text="Exit", command=self.exit_app, font=("Helvetica", 15),
+                             height=1, width=10)
+        low_button = Button(self.master_screen, text="Low Security", command=self.set_to_low_security,
+                            font=("Helvetica", 15),
+                             height=1, width=10)
+        high_button = Button(self.master_screen, text="High Security", command=self.set_to_high_security,
+                             font=("Helvetica", 15),
+                             height=1, width=10)
+        dectection_button = Button(self.master_screen, text="Toggle Object Detection", command=self.toggle_object_detection,
+                             font=("Helvetica", 15),
+                             height=1, width=25)
+
+        # Outputting all widgets
+        # Labels
+        app_title.place(x=175, y=30)
+        # buttons
+        start_button.place(x=0, y=350)
+        exit_button.place(x=480, y=350)
+        low_button.place(x=160, y=140)
+        high_button.place(x=325, y=140)
+        dectection_button.place(x=160, y=210)
+
+        self.master_screen.mainloop()
+
+
+if __name__ == "__main__":
+    while True:
+        options_menu = OptionMenu()
+        options_menu.options_menu()
+        output = Output(options_menu.security_level_input, options_menu.object_detection_input)
+        output.start_video_processing()
